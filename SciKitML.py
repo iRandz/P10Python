4c67108a8933b40f9ae88d758b4d30748fd06d32
iRandz <chrbj-@hotmail.com>
2022-03-14 12:35:39 +0100

    Added neural network, OneVSRest, SVM

--- SciKitML.py
+++ SciKitML.py
@@ -6,21 +6,30 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.feature_selection import RFE
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn import svm
 
 # Settings ----------------------------------------------------------------
-usePCA = 1
 show2D = 0
 show3D = 1
-dimensionality = 3
-neighbors = 3
+
+usePCA = 0
+dimensionalityPCA = 6
+useFeatSel = 1
+dimensionalitySel = 3
+
+neighbors = 3  # KNN
+nnHiddenLayers = (10, 15, 6)  # NN
+ovrEstimator = KNeighborsClassifier(n_neighbors=neighbors)  # OvR
 
 # Prepare Data -------------------------------------------------------------
-#data = pd.read_csv("CombinedData.csv", sep=';')
 data = pd.read_csv("RealData.csv", sep=';')
+#data = pd.read_csv("CombinedData.csv", sep=';')
 
 #data.pop('Weekly playtime')
 data.pop('Participant ID')
@@ -31,10 +40,10 @@ data.pop('Manage mean')
 data.pop('Assault mean')
 data.pop('Unique tiles')
 #data.pop('Deaths')
-data.pop('MajorLore seen')
-data.pop('MajorLore reading time')
-data.pop('MajorLore close')
-data.pop('MajorLore interactions')
+#data.pop('MajorLore seen')
+#data.pop('MajorLore reading time')
+#data.pop('MajorLore close')
+#data.pop('MajorLore interactions')
 
 data_features = data.copy()
 data_labels = data_features.pop('Type')
@@ -50,20 +59,25 @@ if simplify:
 min_max_scaler = preprocessing.MinMaxScaler()
 data_features_norm = min_max_scaler.fit_transform(data_features)
 for y in range(data_features.iloc[0, :].size):
-    for x in range(data_features.iloc[:,0].size):
+    for x in range(data_features.iloc[:, 0].size):
         data_features.iloc[x, y] = data_features_norm[x, y]
 
 # Feature extraction (PCA)
-pca_features = PCA(n_components=dimensionality).fit_transform(data_features)
+pca_features = PCA(n_components=dimensionalityPCA).fit_transform(data_features)
 
 # Feature selection
-selector = SelectKBest(chi2, k=dimensionality) #.fit_transform(data_features, data_labels)
+selector = SelectKBest(chi2, k=dimensionalitySel) #.fit_transform(data_features, data_labels)
 selector.fit(data_features, data_labels)
 cols = selector.get_support(indices=True)
 data_features = data_features.iloc[:, cols]
 
 if usePCA:
     data_features = pca_features
+    if useFeatSel:
+        selector = SelectKBest(k=dimensionalitySel)
+        selector.fit(pca_features, data_labels)
+        cols = selector.get_support(indices=True)
+        data_features = pca_features[:, cols]
 else:
     print(data_features.columns)
 
@@ -75,17 +89,43 @@ for x in range(data_labels.size):
 
 # Create test/train split
 X_train, X_test, y_train, y_test = train_test_split(
-        data_features, data_labels, test_size=0.2, random_state=42
+        data_features, data_labels, test_size=0.4, random_state=42
     )
 
-# Prepare and train model -------------------------------------------------------
+# ML -----------------------------------------------------------------------------------------
+# Prepare and train model
 neigh = KNeighborsClassifier(n_neighbors=neighbors)
 neigh.fit(X_train, y_train)
 
-# Validate model -------------------------------------------------------------------
+neural = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=nnHiddenLayers, random_state=1, max_iter=1000)
+neural.fit(X_train, y_train)
+
+ovr = OneVsRestClassifier(ovrEstimator)
+ovr.fit(X_train, y_train)
+
+svmCLF = svm.SVC()
+svmCLF.fit(X_train, y_train)
+
+# Validate model
+print("K nearest neighbor")
 print(neigh.predict(X_test))
 print(np.array(y_test))
 print(neigh.score(X_test, y_test))
+
+print("\n -----------------------------")
+print("Multi layer perceptron")
+print("Iterations: " + str(neural.n_iter_))
+print("Layers: " + str(neural.hidden_layer_sizes))
+print(neural.score(X_test, y_test))
+
+print("\n -----------------------------")
+print("One versus rest")
+print("Using: " + str(ovr.estimator))
+print(ovr.score(X_test, y_test))
+
+print("\n -----------------------------")
+print("SVM")
+print(svmCLF.score(X_test, y_test))
 
 score = neigh.score(X_test, y_test)
 
