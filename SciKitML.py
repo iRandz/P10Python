967ec40d8bd75fb29acd9bc5977559296cd93c1d
iRandz <chrbj-@hotmail.com>
2022-03-25 14:05:47 +0100

    Tested new data. Updated with capability to recalc manage types. Added 2nd order features. Added balanced score.

--- SciKitML.py
+++ SciKitML.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import sklearn.feature_selection
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -13,24 +14,47 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from sklearn import svm
+from sklearn.metrics import balanced_accuracy_score
 
 # Settings ----------------------------------------------------------------
 show2D = 0
 show3D = 1
+column1X = 0
+column2Y = 1
+column3Z = 2
 
-usePCA = 1
-dimensionalityPCA = 3
-useFeatSel = 0
-dimensionalitySel = 3
+recalcManage = 0
+normalize = 1
 
-neighbors = 3  # KNN
+usePCA = 0
+useFeatSel = 1
+
+dimensionalityPCA = 4
+dimensionalitySel = 4
+useNewFeatSel = 0
+
+neighbors = 5  # KNN
 nnHiddenLayers = (10, 15, 6)  # NN
 ovrEstimator = KNeighborsClassifier(n_neighbors=neighbors)  # OvR
 svmKernel = 'linear'  # SVM
 
+
 # Prepare Data -------------------------------------------------------------
-data = pd.read_csv("RealData.csv", sep=';')
+def RecalcManage(row):
+    if row['Type'] == 'Manage':
+        if row['Assault mean'] > row['Journey mean']:
+            return 'Assault'
+        return 'Journey'
+    return row['Type']
+
+
+#data = pd.read_csv("RealData.csv", sep=';')
 #data = pd.read_csv("CombinedData.csv", sep=';')
+data = pd.read_csv("V2Data.csv", sep=';')
+
+if recalcManage:
+    data['Type'] = data.apply (lambda row: RecalcManage(row), axis=1)
+    #data = data[data.Type != 'Manage']
 
 #data.pop('Weekly playtime')
 data.pop('Participant ID')
@@ -55,13 +79,35 @@ if simplify:
         if data_labels[x] == 'Assault' or data_labels[x] == 'Journey':
             data_labels[x] = 'Other'
 
+
 # Preprocessing ----------------------------------------------------------
+def CalcRatio(row, timeName, interactionName):
+    if row[interactionName] == 0:
+        return 0
+    return row[timeName] / row[interactionName]
+
+
+# Calculate 2nd order features
+data_features['MajorLoreTimePr'] = data_features.apply (lambda row: CalcRatio(row, 'MajorLore reading time', 'MajorLore interactions'), axis=1)
+data_features['LoreTimePr'] = data_features.apply (lambda row: CalcRatio(row, 'Lore reading time', 'Lore interactions'), axis=1)
+#data_features['MajorLoreRatioClose'] = data_features.apply (lambda row: CalcRatio(row, 'MajorLore interactions', 'MajorLore seen'), axis=1)
+data_features['LoreRatioSeen'] = data_features.apply (lambda row: CalcRatio(row, 'Lore interactions', 'Lore seen'), axis=1)
+data_features['LoreRatioClose'] = data_features.apply (lambda row: CalcRatio(row, 'Lore interactions', 'Lore close'), axis=1)
+data_features['ResourceRatioSeen'] = data_features.apply (lambda row: CalcRatio(row, 'Resources', 'Resources seen '), axis=1)
+data_features['ResourceRatioClose'] = data_features.apply (lambda row: CalcRatio(row, 'Resources', 'Resources close'), axis=1)
+data_features['EnemyRatioSeen'] = data_features.apply (lambda row: CalcRatio(row, 'Kills', 'Enemies seen'), axis=1)
+data_features['EnemyRatioClose'] = data_features.apply (lambda row: CalcRatio(row, 'Kills', 'Enemies close'), axis=1)
+data_features['ResourceRatioSeen'] = data_features.apply (lambda row: CalcRatio(row, 'Resources', 'Resources seen '), axis=1)
+#data_features['MapTimePr'] = data_features.apply (lambda row: CalcRatio(row, 'XXX', 'Opened map'), axis=1)
+
+
 # Normalize to 0-1 range
-min_max_scaler = preprocessing.MinMaxScaler()
-data_features_norm = min_max_scaler.fit_transform(data_features)
-for y in range(data_features.iloc[0, :].size):
-    for x in range(data_features.iloc[:, 0].size):
-        data_features.iloc[x, y] = data_features_norm[x, y]
+if normalize:
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data_features_norm = min_max_scaler.fit_transform(data_features)
+    for y in range(data_features.iloc[0, :].size):
+        for x in range(data_features.iloc[:, 0].size):
+            data_features.iloc[x, y] = data_features_norm[x, y]
 
 # Feature extraction (PCA)
 pca_features = PCA(n_components=dimensionalityPCA).fit_transform(data_features)
@@ -69,15 +115,12 @@ pca_Data = PCA(n_components=dimensionalityPCA)
 pca_Data.fit(data_features)
 
 #print("Components: " + str(pca_Data.components_))
+print("PCA:")
 print("Explained variance: " + str(pca_Data.explained_variance_))
 print("Explained variance ratio: " + str(pca_Data.explained_variance_ratio_))
+print("---")
 
 # Feature selection
-selector = SelectKBest(chi2, k=dimensionalitySel) #.fit_transform(data_features, data_labels)
-selector.fit(data_features, data_labels)
-cols = selector.get_support(indices=True)
-data_features = data_features.iloc[:, cols]
-
 if usePCA:
     data_features = pca_features
     if useFeatSel:
@@ -86,13 +129,32 @@ if usePCA:
         cols = selector.get_support(indices=True)
         data_features = pca_features[:, cols]
 else:
+    if useFeatSel:
+        # Feature selection
+        selector = SelectKBest(chi2, k=dimensionalitySel)  # .fit_transform(data_features, data_labels)
+        selector.fit(data_features, data_labels)
+        cols = selector.get_support(indices=True)
+        data_features = data_features.iloc[:, cols]
     print(data_features.columns)
 
 # Force wrongly labelled data into 'Other'
 for x in range(data_labels.size):
-    y = data_labels[x]
+    y = data_labels.iloc[x]
     if y != 'Manage' and y != 'Journey' and y != 'Assault' and y != 'Other':
         data_labels[x] = 'Other'
+
+if useNewFeatSel:
+    knn = KNeighborsClassifier(n_neighbors=3)
+    sfs = sklearn.feature_selection.SequentialFeatureSelector(knn, n_features_to_select=3)
+    sfs.fit(data_features, data_labels)
+    print("---")
+    print(sfs.get_support())
+
+    #sfs.transform(data_features)
+
+    print(data_features)
+    print("---")
+
 
 # Create test/train split
 X_train, X_test, y_train, y_test = train_test_split(
@@ -114,26 +176,36 @@ svmCLF = svm.SVC(kernel=svmKernel)
 svmCLF.fit(X_train, y_train)
 
 # Validate model
+print("\n -----------------------------")
 print("K nearest neighbor")
-print(neigh.predict(X_test))
-print(np.array(y_test))
+# print(neigh.predict(X_test))
+# print(np.array(y_test))
 print(neigh.score(X_test, y_test))
+y_true = y_test
+y_pred = neigh.predict(X_test)
+print(balanced_accuracy_score(y_true, y_pred))
 
 print("\n -----------------------------")
 print("Multi layer perceptron")
 print("Iterations: " + str(neural.n_iter_))
 print("Layers: " + str(neural.hidden_layer_sizes))
 print(neural.score(X_test, y_test))
+y_pred = neural.predict(X_test)
+print(balanced_accuracy_score(y_true, y_pred))
 
 print("\n -----------------------------")
 print("One versus rest")
 print("Using: " + str(ovr.estimator))
 print(ovr.score(X_test, y_test))
+y_pred = ovr.predict(X_test)
+print(balanced_accuracy_score(y_true, y_pred))
 
 print("\n -----------------------------")
 print("SVM")
 print("Kernel: " + str(svmCLF.kernel))
 print(svmCLF.score(X_test, y_test))
+y_pred = svmCLF.predict(X_test)
+print(balanced_accuracy_score(y_true, y_pred))
 
 score = neigh.score(X_test, y_test)
 
@@ -144,11 +216,6 @@ mydict = {'Journey': 'green',
           'Other': 'black'}
 
 if show3D:
-
-    column1X = 0
-    column2Y = 1
-    column3Z = 2
-
     fig = plt.figure(1, figsize=(8, 6))
     ax = fig.add_subplot(projection='3d')
 
@@ -171,9 +238,6 @@ if show3D:
     plt.show()
 
 if show2D:
-    column1X = 0
-    column2Y = 1
-
     if usePCA:
         plt.scatter(pca_features[:, column1X], pca_features[:, column2Y], c=data_labels.map(mydict))
     else:
