34b0b9fc0f9510ce3194a73014bfeb1ab93e64f3
Mathais <mfaldt17@student.aau.dk>
2022-05-01 12:14:44 +0200

    Changed how the seconds data is treated, and the files are now saved
    The files are in 10 sec windows

--- SecondsML.py
+++ SecondsML.py
@@ -6,32 +6,69 @@ import Settings
 import SingleClassification
 import FeatureDict as fD
 
-settings = Settings.Settings()
-# Prepare Data -------------------------------------------------------------
-data = pd.read_csv("Data/CombinedSecondsLog.csv", sep=';')
 
-print("Check for null:")
-print(np.any(pd.isnull(data)))
-print("---")
+if False:
+    settings = Settings.Settings()
+    # Prepare Data -------------------------------------------------------------
+    data = pd.read_csv("Data/CombinedSecondsLog.csv", sep=';')
 
-# Calculate 2nd order features
-data_features: pd.DataFrame = Functions.calc_derived_features(data)
+    print("Check for null:")
+    print(np.any(pd.isnull(data)))
+    print("---")
 
-# Process data
-data_features, data_labels = Functions.process_data(data_features, settings)
+    # Calculate 2nd order features
+    data_features: pd.DataFrame = Functions.calc_derived_features(data)
 
-# Running average
-runningRange = 50
+    # Process data
+    data_features, data_labels = Functions.process_data(data_features, settings)
+    data_features_day = pd.DataFrame(data_features.pop(fD.DAY))
+    data_features_Difference: pd.DataFrame = data_features.copy(True)
+    for i in range(data_features.iloc[:, 0].size):
+        if i == 1:
+            continue
+        currentRow = data_features.iloc[i]
+        previousRow = data_features.iloc[i-1]
+        data_features_Difference.iloc[i] = currentRow-previousRow
 
-data_features_running: pd.DataFrame = data_features.rolling(runningRange).sum()
+    data_features_Difference.to_csv('Differentieret_CombinedSecondsLog.csv', sep=';', index=False)
 
-dropList = range(0, runningRange)
-print(dropList)
-data_features_running = data_features_running.drop(dropList)
-data_labels = data_labels.drop(dropList)
+    # Running average
+    runningRange = 10
 
-print(data_features_running)
+    data_features_running: pd.DataFrame = data_features_Difference.rolling(runningRange).sum()
+    prevDayString = "4"
+    for i in range(data_features_running.iloc[:, 0].size, 0, -1):
+        rowInfo = data_features_running.iloc[i-1]
+        dayRow = data_features_day.iloc[i-1]
+        currentDayString = str(dayRow[fD.DAY])
+        if currentDayString == prevDayString:
+            continue
+        elif currentDayString != prevDayString:
+            dropList = range(i, i + runningRange - 1)
+            data_features_running = data_features_running.drop(dropList)
+            data_labels = data_labels.drop(dropList)
+            prevDayString = currentDayString
 
-data_features_running = Functions.normalize(data_features_running)
+    dropList = range(0, runningRange - 1)
+    data_features_running = data_features_running.drop(dropList)
+    data_labels = data_labels.drop(dropList)
 
-SingleClassification.single_classification(data_features_running, data_labels, settings)
+    data_features_running.to_csv('Running_Differentieret_CombinedSecondsLog.csv', sep=';', index=False)
+    data_labels.to_csv('Running_Lables_CombinedSecondsLog.csv', sep=';', index=False)
+
+    print("---/n Data processing done")
+
+if __name__ == '__main__':
+    print("Only Main")
+    settings = Settings.Settings()
+
+    data_features_running = pd.read_csv("Running_Differentieret_CombinedSecondsLog.csv", sep=';')
+    data_labels = pd.read_csv("Running_Lables_CombinedSecondsLog.csv", sep=';')
+
+    print("Check for null:")
+    print(np.any(pd.isnull(data_features_running)))
+    print("---")
+
+    data_features_running = Functions.normalize(data_features_running)
+    print("Running classification")
+    SingleClassification.single_classification(data_features_running, data_labels, settings)
